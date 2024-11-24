f68004acb5fae38343941ee2a57ac8cf63277ae8
Thandolwethu <thandwem045@gmail.com>
2024-11-24 12:20:34 +0200

    Added types to reviews array

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,34 @@ const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
 
-const reviews = [
-    {
-        name: 'Sheia',
-        stars: 5,
-        loyaltyUser: true,
-        date: '01-04-2021'
-    },
-    {
-        name: 'Andrzej',
-        stars: 3,
-        loyaltyUser: false,
-        date: '28-03-2021'
-    },
-    {
-        name: 'Omar',
-        stars: 4,
-        loyaltyUser: true,
-        date: '27-03-2021'
-    },
-]
+const reviews: {
+    name: string;
+    stars: number;
+    loyaltyUser: boolean;
+    date: string;
+
+}[
+
+] = [
+        {
+            name: 'Sheia',
+            stars: 5,
+            loyaltyUser: true,
+            date: '01-04-2021'
+        },
+        {
+            name: 'Andrzej',
+            stars: 3,
+            loyaltyUser: false,
+            date: '28-03-2021'
+        },
+        {
+            name: 'Omar',
+            stars: 4,
+            loyaltyUser: true,
+            date: '27-03-2021'
+        },
+    ]
 
 
 function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
@@ -31,25 +39,29 @@ function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
+
 //Adds naam of user on the welcome page
 const you: {
-    firstname: string,
-    lastName: string,
-    isReturning: true,
-    age: number
+    firstname: string;
+    lastName: string;
+    isReturning: true;
+    age: number;
+    stayedAt: string[];
+
 } = {
     firstname: 'Bobby',
     lastName: 'Brown',
     isReturning: true,
-    age: 23
+    age: 23,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
 
-function populateUser(isReturning: boolean, userName: string) {
+function populateUser(isReturning: boolean, firstname: string) {
     if (isReturning) {
         returningUserDisplay.innerHTML = 'Back'
     }
-    userNameDisplay.innerHTML = userName
+    userNameDisplay.innerHTML = firstname
 }
 
-populateUser(you.isReturning, you.userName)
+populateUser(you.isReturning, you.firstname)
